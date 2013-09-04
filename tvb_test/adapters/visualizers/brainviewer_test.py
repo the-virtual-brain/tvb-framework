c82c7b3eff078cd7fee32e8fb421d3e742ad97dc
lia.domide <lia.domide@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2013-09-04 10:55:51 +0000

    Fix test after viewer adapter changed.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/framework_tvb@5092 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb_test/adapters/visualizers/brainviewer_test.py
+++ tvb_test/adapters/visualizers/brainviewer_test.py
@@ -109,9 +109,8 @@ class BrainViewerTest(TransactionalTestCase):
         time_series = self.datatypeFactory.create_timeseries(self.connectivity)
         viewer = BrainViewer()
         result = viewer.generate_preview(time_series, (500, 200))
-        expected_keys = ['urlVertices', 'urlNormals', 'urlTriangles', 'pageSize',
-                         'minActivityLabels', 'minActivity', 'maxActivity', 'isOneToOneMapping',
-                         'isAdapter', 'base_activity_url', 'alphas_indices']
+        expected_keys = ['urlVertices', 'urlNormals', 'urlTriangles', 'minActivityLabels', 'minActivity', 'maxActivity',
+                         'isOneToOneMapping', 'isAdapter', 'base_activity_url', 'alphas_indices']
         for key in expected_keys:
             self.assertTrue(key in result and result[key] is not None)
         
