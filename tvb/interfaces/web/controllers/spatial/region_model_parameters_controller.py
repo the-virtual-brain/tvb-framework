33facc40c690892e91a256da7026a088237364f6
mihai.andrei <mihai.andrei@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2014-04-28 08:50:10 +0000

    TVB-1081 Added equations to the model  exploration interface
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/framework_tvb@5898 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb/interfaces/web/controllers/spatial/region_model_parameters_controller.py
+++ tvb/interfaces/web/controllers/spatial/region_model_parameters_controller.py
@@ -55,6 +55,59 @@ class RegionsModelParametersController(SpatioTemporalController):
         SpatioTemporalController.__init__(self)
 
 
+    def _dfun_math_directives_to_matjax(self, model):
+        """
+        Looks for sphinx math directives if the docstring of the dfun function of a model.
+        It converts them in html text that will be interpreted by mathjax
+        The parsing is simplistic, not a rst parser.
+        Wraps .. math :: body in \[\begin{split}\end{split}\]
+        """
+
+        def parse(doc):
+            """
+            parse docstrings for .. math :: directive
+            """
+            # doc = text | math
+            BEGIN = r'\[\begin{split}'
+            END = r'\end{split}\]'
+            in_math = False
+            out_lines = []
+            indent = ''
+
+            for line in doc.splitlines():
+                if not in_math:
+                    # math = indent directive math_body
+                    indent, sep, _ = line.partition('.. math::')
+                    if sep:
+                        out_lines.append(BEGIN)
+                        in_math = True
+                    else:
+                        out_lines.append(line)
+                else:
+                    # math body is at least 1 space more indented than the directive, but we tolerate empty lines
+                    if line.startswith(indent + ' ') or line.strip() == '':
+                        out_lines.append(line)
+                    else:
+                        # this line is not properly indented, math block is over
+                        out_lines.append(END)
+                        out_lines.append(line)
+                        in_math = False
+
+            if in_math:
+                # close math tag
+                out_lines.append(END)
+
+            return '\n'.join(out_lines)
+
+
+        dfun = getattr(model, 'dfun', None)
+
+        if dfun:
+            return parse(dfun.__doc__).replace('&', '&amp;').replace('.. math::','')
+        else:
+            return ''
+
+
     @expose_page
     def edit_model_parameters(self):
         """
@@ -76,6 +129,7 @@ class RegionsModelParametersController(SpatioTemporalController):
         template_specification['mainContent'] = 'spatial/model_param_region_main'
         template_specification['displayDefaultSubmitBtn'] = True
         template_specification.update(context_model_parameters.phase_plane_params)
+        template_specification['modelEquations'] = self._dfun_math_directives_to_matjax(model)
         return self.fill_default_attributes(template_specification)
 
 
