14e7ecf37e910be85927d579e775a2d0b6fde377
lia.domide <lia.domide@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2014-05-09 12:28:52 +0000

    Small code review.
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/framework_tvb@5975 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb/interfaces/web/controllers/api/burst.py
+++ tvb/interfaces/web/controllers/api/burst.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2013, Baycrest Centre for Geriatric Care ("Baycrest")
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation. This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details. You should have received a copy of the GNU General
+# Public License along with this program; if not, you can download it here
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
 
 """
 Providing the Burst services as an HTTP/JSON API.
@@ -6,20 +35,18 @@ Providing the Burst services as an HTTP/JSON API.
 
 import json
 import cherrypy
-import tvb.interfaces.web.controllers.basecontroller as base
-from tvb.core.services.burstservice import BurstService
-
-# simulator imports 
-from tvb.datatypes import connectivity, equations, surfaces, patterns
-from tvb.simulator import noise, integrators, models, coupling, monitors, simulator
-
+import tvb.interfaces.web.controllers.base_controller as base
 import tvb.interfaces.web.controllers.api.burst_impl as impl
+from tvb.core.services.burst_service import BurstService
+from tvb.datatypes import connectivity, equations, surfaces, patterns
+from tvb.simulator import noise, integrators, models, coupling, monitors
+
+
 
 class BurstAPIController(base.BaseController):
     """
     Provides an HTTP/JSON API to the simulator/burst mechanics
     reusing a BurstController where necessary.
-
     """
 
     exposed = True
@@ -28,27 +55,28 @@ class BurstAPIController(base.BaseController):
         super(BurstAPIController, self).__init__()
         self.burst_service = BurstService()
 
+
     @cherrypy.expose
     def index(self):
 
         reload(impl)
         return impl.index(self)
 
+
     @cherrypy.expose
     def read(self, pid):
         """
         Get information on existing burst operations
-
         """
 
         reload(impl)
         return impl.read(self, pid)
 
+
     @cherrypy.expose
     def dir(self):
         """
         Query existing classes in TVB
-
         """
 
         info = {}
@@ -63,11 +91,11 @@ class BurstAPIController(base.BaseController):
 
         return json.dumps(info)
 
+
     @cherrypy.expose
     def create(self, opt):
         """
         Create a new simulation/burst
-
         """
 
         reload(impl)
--- tvb/interfaces/web/controllers/api/burst_impl.py
+++ tvb/interfaces/web/controllers/api/burst_impl.py
@@ -1,3 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2013, Baycrest Centre for Geriatric Care ("Baycrest")
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the Free
+# Software Foundation. This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details. You should have received a copy of the GNU General
+# Public License along with this program; if not, you can download it here
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
 
 """
 Reloadable module of functions that implements burst API.
@@ -8,7 +37,7 @@ This is separated in order to speed development
 
 import json 
 
-import tvb.interfaces.web.controllers.basecontroller as base
+import tvb.interfaces.web.controllers.base_controller as base
 
 """
 model_parameters_option_Generic2dOscillator_state_variable_range_parameters_W
@@ -41,6 +70,7 @@ model_parameters_option_Generic2dOscillator_variables_of_interest
 model
 """
 
+
 class HierStruct(object):
     """
     Class to handle by flat and nested indexing of simulator configuration
@@ -54,13 +84,14 @@ class HierStruct(object):
 def index(self):
     return 'Burst API'
 
+
 def read(self, pid):
     pid = int(pid)
     info = {}
     bursts = self.burst_service.get_available_bursts(pid)
     import pdb; pdb.set_trace()
     for burst in bursts:
-        info[burst.name] = {k:v for k,v in burst.simulator_configuration.iteritems() if len(k)>0}
+        info[burst.name] = {k: v for k, v in burst.simulator_configuration.iteritems() if len(k) > 0}
     return json.dumps(info)
 
 
