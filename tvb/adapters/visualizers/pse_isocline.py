63619e6a58a84f7e7b0ebe95802da6970ac8707e
mihai.andrei <mihai.andrei@1c0e02f0-7929-43c0-8fb3-3293bf43b0d1>
2014-05-13 13:44:04 +0000

    TVB-1164 Separated data gathering from view logic. Improved performance by not repeating data gathering for each metric
    git-svn-id: https://repo.thevirtualbrain.org/svn/tvb/trunk/framework_tvb@5983 1c0e02f0-7929-43c0-8fb3-3293bf43b0d1

--- tvb/adapters/visualizers/pse_isocline.py
+++ tvb/adapters/visualizers/pse_isocline.py
@@ -36,18 +36,134 @@ import sys
 import numpy
 import json
 from scipy import interpolate
-from tvb.core.entities import model
+from tvb.basic.logger.builder import get_logger
+from tvb.core.entities.model import DataTypeGroup, OperationGroup, STATUS_STARTED
 from tvb.core.entities.storage import dao
 from tvb.core.adapters.abcdisplayer import ABCMPLH5Displayer
 from tvb.core.adapters.exceptions import LaunchException
 from tvb.datatypes.mapped_values import DatatypeMeasure
-from tvb.basic.config.settings import TVBSettings as config
+from tvb.basic.config.settings import TVBSettings
 from tvb.basic.filters.chain import FilterChain
 
 
 # The resolution for computing dots inside the displayed isocline.
 # This is not the sae as display size.
 RESOLUTION = (600, 600)
+
+
+class PseIsoModel(object):
+
+    def __init__(self, range1, range2, apriori_data, metrics, datatype_gids):
+        self.log = get_logger(self.__class__.__name__)
+        # ranges
+        all_numbers_range1, self.range1_name, self.range1 = OperationGroup.load_range_numbers(range1)
+        all_numbers_range2, self.range2_name, self.range2 = OperationGroup.load_range_numbers(range2)
+
+        # Data from which to interpolate larger 2-D space
+        self.apriori_x = self._prepare_axes(self.range1, all_numbers_range1)
+        self.apriori_y = self._prepare_axes(self.range2, all_numbers_range2)
+        self.apriori_data = apriori_data
+        self.datatypes_gids = datatype_gids
+        self.metrics = metrics
+
+    @classmethod
+    def from_db(cls, operation_group_id):
+        """
+        Collects from db the information about the operation group that is required by the isocline view.
+        """
+        operations = dao.get_operations_in_group(operation_group_id)
+        operation_group = dao.get_operationgroup_by_id(operation_group_id)
+
+        self = cls(operation_group.range1, operation_group.range2, {},
+                   PseIsoModel._find_metrics(operations), None)
+
+        self._fill_apriori_data(operations)
+        # self.log.warning(self.as_json())
+        return self
+
+
+    @staticmethod
+    def _find_metrics(operations):
+        """ Search for an operation with results. Then get the metrics of the generated data type"""
+        dt_measure = None
+
+        for operation in operations:
+
+            if operation.status == STATUS_STARTED:
+                raise LaunchException("Can not display until all operations from this range are finished!")
+
+            op_results = dao.get_results_for_operation(operation.id)
+            if len(op_results):
+                datatype = op_results[0]
+                if datatype.type == "DatatypeMeasure":
+                    ## Load proper entity class from DB.
+                    dt_measure = dao.get_generic_entity(DatatypeMeasure, datatype.id)[0]
+                else:
+                    dt_measure = dao.get_generic_entity(DatatypeMeasure, datatype.gid, '_analyzed_datatype')
+                    if dt_measure:
+                        dt_measure = dt_measure[0]
+                break
+
+        if dt_measure:
+            return dt_measure.metrics
+        else:
+            raise LaunchException("No datatypes were generated due to simulation errors. Nothing to display.")
+
+
+    def _fill_apriori_data(self, operations):
+        """ Gather apriori data from the operations. Also gather the datatype gid's"""
+        for metric in self.metrics:
+            self.apriori_data[metric] = numpy.zeros((self.apriori_x.size, self.apriori_y.size))
+
+        # An 2D array of GIDs which is used later to launch overlay for a DataType
+        self.datatypes_gids = [[None for _ in self.range2] for _ in self.range1]
+
+        for operation in operations:
+            self.log.info("Gathering data from operation : %s" % operation.id)
+            range_values = eval(operation.range_values)
+            key_1 = range_values[self.range1_name]
+            index_x = self.range1.index(key_1)
+            key_2 = range_values[self.range2_name]
+            index_y = self.range2.index(key_2)
+            if operation.status == STATUS_STARTED:
+                raise LaunchException("Not all operations from this range are complete. Cannot view until then.")
+
+            operation_results = dao.get_results_for_operation(operation.id)
+            if operation_results:
+                datatype = operation_results[0]
+                self.datatypes_gids[index_x][index_y] = datatype.gid
+
+                if datatype.type == "DatatypeMeasure":
+                    measures = dao.get_generic_entity(DatatypeMeasure, datatype.id)
+                else:
+                    measures = dao.get_generic_entity(DatatypeMeasure, datatype.gid, '_analyzed_datatype')
+            else:
+                self.datatypes_gids[index_x][index_y] = None
+                measures = None
+
+            for metric in self.metrics:
+                if measures:
+                    self.apriori_data[metric][index_x][index_y] = measures[0].metrics[metric]
+                else:
+                    self.apriori_data[metric][index_x][index_y] = numpy.NaN
+
+
+    @staticmethod
+    def _prepare_axes(original_range_values, is_numbers):
+        result = numpy.array(original_range_values)
+        if not is_numbers:
+            result = numpy.arange(len(original_range_values))
+        return result
+
+    def as_json(self):
+        de_numpy_ed = dict( (k, v.tolist()) for k,v in self.apriori_data.iteritems())
+        return json.dumps({
+            'apriori_data': de_numpy_ed,
+            'metrics': self.metrics,
+            'datatypes_gids': self.datatypes_gids,
+            'range1':json.dumps([self.range1_name, self.range1]),
+            'range2':json.dumps([self.range2_name, self.range2])
+        })
 
 
 
@@ -75,7 +191,7 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
         """
         return [{'name': 'datatype_group',
                  'label': 'Datatype Group',
-                 'type': model.DataTypeGroup,
+                 'type': DataTypeGroup,
                  'required': True,
                  'conditions': FilterChain(fields=[FilterChain.datatype + ".no_of_ranges"],
                                            operations=["=="], values=[2])}]
@@ -117,86 +233,29 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
         else:
             figsize = (15, 7)
 
-        operation_group = dao.get_operationgroup_by_id(datatype_group.fk_operation_group)
-        self.all_numbers_range1, range1_name, self.range1 = operation_group.load_range_numbers(operation_group.range1)
-        self.all_numbers_range2, range2_name, self.range2 = operation_group.load_range_numbers(operation_group.range2)
-
-        for operation in dao.get_operations_in_group(operation_group.id):
-            if operation.status == model.STATUS_STARTED:
-                raise LaunchException("Can not display until all operations from this range are finished!")
-
-            op_results = dao.get_results_for_operation(operation.id)
-            if len(op_results):
-                datatype = op_results[0]
-                if datatype.type == "DatatypeMeasure":
-                    ## Load proper entity class from DB.
-                    dt_measure = dao.get_generic_entity(DatatypeMeasure, datatype.id)[0]
-                else:
-                    dt_measure = dao.get_generic_entity(DatatypeMeasure, datatype.gid, '_analyzed_datatype')
-                    if dt_measure:
-                        dt_measure = dt_measure[0]
-                break
-            else:
-                dt_measure = None
+        model = PseIsoModel.from_db(datatype_group.fk_operation_group)
 
         figure_nrs = {}
-        metrics = dt_measure.metrics if dt_measure else {}
-        if metrics:
-            for metric in metrics:
-                # Separate plot for each metric.
-                self._create_plot(metric, figsize, operation_group, range1_name, range2_name, figure_nrs)
-        else:
-            raise LaunchException("No datatypes were generated due to simulation errors. Nothing to display.")
+        for metric in model.metrics:
+            # Separate plot for each metric.
+            self._create_plot(model, metric, figsize, figure_nrs)
 
         parameters = dict(title=self._ui_name, showFullToolbar=True,
-                          serverIp=config.SERVER_IP, serverPort=config.MPLH5_SERVER_PORT,
-                          figureNumbers=figure_nrs, metrics=metrics, figuresJSON=json.dumps(figure_nrs))
+                          serverIp=TVBSettings.SERVER_IP, serverPort=TVBSettings.MPLH5_SERVER_PORT,
+                          figureNumbers=figure_nrs, metrics=model.metrics, figuresJSON=json.dumps(figure_nrs))
 
         return self.build_display_result("pse_isocline/view", parameters)
 
 
-    def plot(self, figure, operation_group, metric, range1_name, range2_name):
+    def _plot(self, figure, model, metric):
         """
         Do the plot for the given figure. Also need operation group, metric and ranges
         in order to compute the data to be plotted.
         """
-        operations = dao.get_operations_in_group(operation_group.id)
-        # Data from which to interpolate larger 2-D space
-        apriori_x = self._prepare_axes(self.range1, self.all_numbers_range1)
-        apriori_y = self._prepare_axes(self.range2, self.all_numbers_range2)
-        apriori_data = numpy.zeros((apriori_x.size, apriori_y.size))
-
-        # An 2D array of GIDs which is used later to launch overlay for a DataType
-        datatypes_gids = [[None for _ in self.range2] for _ in self.range1]
-        for operation_ in operations:
-            range_values = eval(operation_.range_values)
-            key_1 = range_values[range1_name]
-            index_x = self.range1.index(key_1)
-            key_2 = range_values[range2_name]
-            index_y = self.range2.index(key_2)
-            if operation_.status == model.STATUS_STARTED:
-                raise LaunchException("Not all operations from this range are complete. Cannot view until then.")
-
-            operation_results = dao.get_results_for_operation(operation_.id)
-            if operation_results:
-                datatype = operation_results[0]
-                datatypes_gids[index_x][index_y] = datatype.gid
-    
-                if datatype.type == "DatatypeMeasure":
-                    measures = dao.get_generic_entity(DatatypeMeasure, datatype.id)
-                else:
-                    measures = dao.get_generic_entity(DatatypeMeasure, datatype.gid, '_analyzed_datatype')
-            else:
-                datatypes_gids[index_x][index_y] = None
-                measures = None
-
-            if measures:
-                apriori_data[index_x][index_y] = measures[0].metrics[metric]
-            else:
-                apriori_data[index_x][index_y] = numpy.NaN
             
         # Convert array to 0 but keep track of nan values so we can replace after interpolation
         # since interpolating with nan values will just break the whole process
+        apriori_data = model.apriori_data[metric]
         nan_indices = numpy.isnan(apriori_data)
         self.nan_indices[figure.number] = nan_indices
         apriori_data = numpy.nan_to_num(apriori_data)
@@ -204,15 +263,15 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
         # For the time being, correctness wins over beauty. The plot will not be as smooth as it
         # was, but it will be sufficiently correct. 
         kx = ky = 1
-        s = interpolate.RectBivariateSpline(apriori_x, apriori_y, apriori_data, kx=kx, ky=ky)
+        s = interpolate.RectBivariateSpline(model.apriori_x, model.apriori_y, apriori_data, kx=kx, ky=ky)
         # Get data of higher resolution that we'll plot later on
-        posteriori_x = numpy.arange(apriori_x[0], apriori_x[-1],
-                                    float(apriori_x[-1] - apriori_x[0]) / RESOLUTION[0])
-        posteriori_y = numpy.arange(apriori_y[0], apriori_y[-1],
-                                    float(apriori_y[-1] - apriori_y[0]) / RESOLUTION[1])
+        posteriori_x = numpy.arange(model.apriori_x[0], model.apriori_x[-1],
+                                    float(model.apriori_x[-1] - model.apriori_x[0]) / RESOLUTION[0])
+        posteriori_y = numpy.arange(model.apriori_y[0], model.apriori_y[-1],
+                                    float(model.apriori_y[-1] - model.apriori_y[0]) / RESOLUTION[1])
         posteriori_data = s(posteriori_x, posteriori_y)
-        x_granularity = RESOLUTION[0] / len(self.range1)
-        y_granularity = RESOLUTION[1] / len(self.range2)
+        x_granularity = RESOLUTION[0] / len(model.range1)
+        y_granularity = RESOLUTION[1] / len(model.range2)
         for idx, row in enumerate(nan_indices):
             for idy, was_nan in enumerate(row):
                 if was_nan:
@@ -233,44 +292,38 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
         self.interp_models[figure.number] = s
         # Do actual plot.        
         axes = figure.gca()
-        img = axes.imshow(posteriori_data, extent=(min(apriori_x), max(apriori_x),
-                                                   min(apriori_y), max(apriori_y)),
+        img = axes.imshow(posteriori_data, extent=(min(model.apriori_x), max(model.apriori_x),
+                                                   min(model.apriori_y), max(model.apriori_y)),
                           aspect='auto', interpolation='bilinear')
         axes.set_title("Interpolated values for metric %s" % (metric,))
         figure.colorbar(img)
-        axes.set_xlabel(range1_name)
-        axes.set_ylabel(range2_name)
+        axes.set_xlabel(model.range1_name)
+        axes.set_ylabel(model.range2_name)
 
 
         def format_coord(x, y):
             return 'x=%1.4f, y=%1.4f' % (x, y)
 
-
         axes.format_coord = format_coord
-        return datatypes_gids
 
 
-    def _prepare_axes(self, original_range_values, is_numbers):
-
-        result = numpy.array(original_range_values)
-
-        if not is_numbers:
-            result = numpy.arange(len(original_range_values))
-
-        return result
-
-
-    def _create_plot(self, metric, figsize, operation_group, range1_name, range2_name, figure_nrs):
+    def _create_plot(self, model, metric, figsize, figure_nrs):
         """
         Create a plot for each metric, with a given figsize:. We need also operation group,
         ranges for data computations. figure_nrs iw a mapping between metric : figure_number
         """
         figure = self._create_new_figure(figsize)
-        
+
+        # Make sure that the callbacks do not close upon the model (will keep the memory hungry model alive)
+        # Save some values needed by the on_click events
+        range1 = model.range1
+        range2 = model.range2
+        datatype_gids = model.datatypes_gids
+
         def _get_x_index(x):
             x_idx = -1
             x_dist = sys.maxint
-            for idx, val in enumerate(self.range1):
+            for idx, val in enumerate(range1):
                 if x_dist > abs(val - x):
                     x_dist = abs(val - x)
                     x_idx = idx
@@ -279,7 +332,7 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
         def _get_y_index(y):
             y_idx = -1
             y_dist = sys.maxint
-            for idx, val in enumerate(self.range2):
+            for idx, val in enumerate(range2):
                 if y_dist > abs(val - y):
                     y_dist = abs(val - y)
                     y_idx = idx
@@ -291,8 +344,8 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
                 x, y = event.xdata, event.ydata
                 x_idx = _get_x_index(x)
                 y_idx = _get_y_index(y)
-                if self.datatype_gids[x_idx][y_idx]:
-                    figure.command = "clickedDatatype('%s')" % (self.datatype_gids[x_idx][y_idx])
+                if datatype_gids[x_idx][y_idx]:
+                    figure.command = "clickedDatatype('%s')" % (datatype_gids[x_idx][y_idx])
 
 
         def _hover_event(event, figure=figure):
@@ -306,10 +359,7 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
                     hover_value = self.interp_models[figure.number]([x], [y])
                 figure.command = "hoverPlot(%s, %s, %s, %s)" % (figure.number, x, y, hover_value)
 
-
-        dt_gids = self.plot(figure, operation_group, metric, range1_name, range2_name)
-        # Save the array of GIDs so we can later look op for the on_click events
-        self.datatype_gids = dt_gids
+        self._plot(figure, model, metric)
         self.figures[figure.number] = figure
         figure_nrs[metric] = figure.number
         figure.canvas.mpl_connect('button_press_event', _click_event)
@@ -317,3 +367,5 @@ class IsoclinePSEAdapter(ABCMPLH5Displayer):
         figure.canvas.draw()
 
 
+    def plot(self, figure, **kwargs):
+        raise Exception("isocline should not use plot")
