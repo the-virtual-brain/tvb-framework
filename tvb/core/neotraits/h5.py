9e40d8bd584c85226bd31ead5a33c79f4cedac9d
mihai.andrei <mihai.andrei@codemart.ro>
2019-03-04 02:52:47 +0200

    neotraitsh5: refactor create public api h5.py

---
+++ tvb/core/neotraits/h5.py
@@ -0,0 +1,7 @@
+from ._h5core import H5File
+from ._h5core import DataSet, DataSetMetaData
+from ._h5core import Scalar, Reference, Accessor
+
+from ._h5accessors import SparseMatrix, SparseMatrixMetaData
+from ._h5accessors import Json
+
