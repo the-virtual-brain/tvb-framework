55419cd1925eba6f85367bedd13ca6a512f6d752
mihai.andrei <mihai.andrei@codemart.ro>
2019-03-09 17:04:32 +0200

    reorganised code that provides the command interface for the h5 loading

--- tvb/interfaces/neocom/_h5loader.py
+++ tvb/interfaces/neocom/_h5loader.py
@@ -5,75 +5,54 @@ import uuid
 import typing
 from tvb.basic.neotraits.api import HasTraits
 from tvb.core.entities.file.hdf5_storage_manager import HDF5StorageManager
+from .config import registry
 
 if typing.TYPE_CHECKING:
     from tvb.core.neotraits.h5 import H5File
 
 
-class Registry(object):
-    def __init__(self):
-        self._datatype_for_h5file = {}
-        self._h5file_for_datatype = {}
-
-    def get_h5file_for_datatype(self, datatype_class):
-        # type: (typing.Type[HasTraits]) -> typing.Type[H5File]
-        return self._h5file_for_datatype[datatype_class]
-
-    def get_datatype_for_h5file(self, h5file_class):
-        # type: (typing.Type[H5File]) -> typing.Type[HasTraits]
-        return self._datatype_for_h5file[h5file_class]
-
-    def register_h5file_datatype(self, h5file_class, datatype_class):
-        self._h5file_for_datatype[datatype_class] = h5file_class
-        self._datatype_for_h5file[h5file_class] = datatype_class
-
+def get_h5file_class(storage_manager):
+    # type: (HDF5StorageManager) -> typing.Type[H5File]
+    meta = storage_manager.get_metadata()
+    h5file_class_fqn = meta.get('written_by')
+    package, cls_name = h5file_class_fqn.rsplit('.', 1)
+    module = importlib.import_module(package)
+    return getattr(module, cls_name)
 
 
 class Loader(object):
     """
     A default simple loader. Does not do recursive loads. Loads stores just to paths.
     """
-    def __init__(self, registry):
-        self.registry = registry
-
-
-    @staticmethod
-    def get_h5file_class(storage_manager):
-        # type: (HDF5StorageManager) -> typing.Type[H5File]
-        meta = storage_manager.get_metadata()
-        h5file_class_fqn = meta.get('written_by')
-        package, cls_name = h5file_class_fqn.rsplit('.', 1)
-        module = importlib.import_module(package)
-        return getattr(module, cls_name)
 
     def load(self, source):
         # type: (str) -> HasTraits
         base_dir, fname = os.path.split(source)
         storage_manager = HDF5StorageManager(base_dir, fname)
-        h5file_cls = self.get_h5file_class(storage_manager)
+        h5file_cls = get_h5file_class(storage_manager)
 
         with h5file_cls(os.path.join(base_dir, fname)) as f:
-            datatype_cls = self.registry.get_datatype_for_h5file(h5file_cls)
+            datatype_cls = registry.get_datatype_for_h5file(h5file_cls)
             datatype = datatype_cls()
             f.load_into(datatype)
             return datatype
 
     def store(self, datatype, destination):
         # type: (HasTraits, str) -> None
-        h5file_cls = self.registry.get_h5file_for_datatype(type(datatype))
+        h5file_cls = registry.get_h5file_for_datatype(type(datatype))
 
         with h5file_cls(destination) as f:
             f.store(datatype)
 
 
 
-class DirLoader(Loader):
+class DirLoader(object):
     """
     A simple recursive loader. Stores all files in a directory.
     You refer to files by their gid
     """
-    def __init__(self, base_dir, registry, recursive=False):
-        Loader.__init__(self, registry)
+    def __init__(self, base_dir, recursive=False):
+        # type: (str, bool) -> None
         if not os.path.isdir(base_dir):
             raise IOError('not a directory {}'.format(base_dir))
 
@@ -96,12 +75,12 @@ class DirLoader(Loader):
 
         fname = self._locate(gid)
         storage_manager = HDF5StorageManager(self.base_dir, fname)
-        h5file_cls = self.get_h5file_class(storage_manager)
+        h5file_cls = get_h5file_class(storage_manager)
 
         sub_dt_refs = []
 
         with h5file_cls(os.path.join(self.base_dir, fname)) as f:
-            datatype_cls = self.registry.get_datatype_for_h5file(h5file_cls)
+            datatype_cls = registry.get_datatype_for_h5file(h5file_cls)
             datatype = datatype_cls()
             f.load_into(datatype)
 
@@ -117,7 +96,7 @@ class DirLoader(Loader):
 
     def store(self, datatype):
         # type: (HasTraits) -> None
-        h5file_cls = self.registry.get_h5file_for_datatype(type(datatype))
+        h5file_cls = registry.get_h5file_for_datatype(type(datatype))
         fname = '{}_{}.h5'.format(type(datatype).__name__, datatype.gid)
 
         sub_dt_refs = []
@@ -126,7 +105,7 @@ class DirLoader(Loader):
             f.store(datatype)
 
             if self.recursive:
-                sub_dt_refs = self._gather_references(f)
+                sub_dt_refs = f.gather_references()
 
         for fname, sub_gid in sub_dt_refs:
             subdt = getattr(datatype, fname)
--- tvb/interfaces/neocom/h5.py
+++ tvb/interfaces/neocom/h5.py
@@ -5,36 +5,67 @@ from tvb.core.entities.file.datatypes.projections_h5 import ProjectionMatrixH5
 from tvb.core.entities.file.datatypes.region_mapping_h5 import RegionMappingH5, RegionVolumeMappingH5
 from tvb.core.entities.file.datatypes.sensors_h5 import SensorsH5
 from tvb.core.entities.file.datatypes.simulation_state_h5 import SimulationStateH5
-from tvb.core.entities.file.datatypes.spectral_h5 import CoherenceSpectrumH5, ComplexCoherenceSpectrumH5, FourierSpectrumH5, WaveletCoefficientsH5
+from tvb.core.entities.file.datatypes.spectral_h5 import (
+    CoherenceSpectrumH5, ComplexCoherenceSpectrumH5,
+    FourierSpectrumH5, WaveletCoefficientsH5
+)
 from tvb.core.entities.file.datatypes.structural_h5 import StructuralMRIH5
 from tvb.core.entities.file.datatypes.surface_h5 import SurfaceH5
 from tvb.core.entities.file.datatypes.temporal_correlations_h5 import CrossCorrelationH5
-from tvb.core.entities.file.datatypes.time_series import TimeSeriesH5, TimeSeriesRegionH5, TimeSeriesSurfaceH5, TimeSeriesVolumeH5
+from tvb.core.entities.file.datatypes.time_series import (
+    TimeSeriesH5, TimeSeriesRegionH5,
+    TimeSeriesSurfaceH5, TimeSeriesVolumeH5
+)
 from tvb.core.entities.file.datatypes.tracts_h5 import TractsH5
 from tvb.core.entities.file.datatypes.volumes_h5 import VolumeH5
-from tvb.interfaces.neocom import _h5impl
 from tvb.interfaces.neocom._h5loader import Loader, DirLoader
 
+import typing
 
-_registry = _h5impl.build_registry()
+if typing.TYPE_CHECKING:
+    from tvb.basic.neotraits.api import HasTraits
+    import uuid
 
 
 def load(source):
-    loader = Loader(_registry)
+    # type: (str) -> HasTraits
+    """
+    Load a datatype stored in the tvb h5 file found at the given path
+    """
+    loader = Loader()
     return loader.load(source)
 
 
 def store(datatype, destination):
-    loader = Loader(_registry)
+    # type: (HasTraits, str) -> None
+    """
+    Stores the given datatype in a tvb h5 file at the given path
+    """
+    loader = Loader()
     return loader.store(datatype, destination)
 
 
 def load_from_dir(base_dir, gid, recursive=False):
-    loader = DirLoader(base_dir, _registry, recursive)
+    # type: (str, typing.Union[str, uuid.UUID], bool) -> HasTraits
+    """
+    Loads a datatype with the requested gid from the given directory.
+    The datatype should have been written with store_to_dir
+    The name and location of the file is chosen for you.
+    :param base_dir:  The h5 storage directory
+    :param gid: the gid of the to be loaded datatype
+    :param recursive: if datatypes contained in this datatype should be loaded as well
+    """
+    loader = DirLoader(base_dir, recursive)
     return loader.load(gid)
 
 
 def store_to_dir(base_dir, datatype, recursive=False):
-    loader = DirLoader(base_dir, _registry, recursive)
+    # type: (str, HasTraits, bool) -> None
+    """
+    Stores the given datatype in the given directory.
+    The name and location of the stored file(s) is chosen for you by this function.
+    If recursive is true than datatypes referenced by this one are stored as well.
+    """
+    loader = DirLoader(base_dir, recursive)
     loader.store(datatype)
 
