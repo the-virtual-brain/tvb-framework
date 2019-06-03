a3ad875480e600a7d3c0210810aa0a6959ba97b9
popaula937 <paula.popa@codemart.ro>
2019-06-03 11:35:32 +0300

    Keep Indexes inside the neocom registry.

--- tvb/interfaces/neocom/config.py
+++ tvb/interfaces/neocom/config.py
@@ -39,34 +39,50 @@ from tvb.core.entities.file.datatypes.time_series import (
     TimeSeriesEEGH5, TimeSeriesMEGH5, TimeSeriesSEEGH5)
 from tvb.core.entities.file.datatypes.tracts_h5 import TractsH5
 from tvb.core.entities.file.datatypes.volumes_h5 import VolumeH5
+from tvb.core.entities.model.datatypes.connectivity import ConnectivityIndex
+from tvb.core.entities.model.datatypes.graph import ConnectivityMeasureIndex
+from tvb.core.entities.model.datatypes.local_connectivity import LocalConnectivityIndex
+from tvb.core.entities.model.datatypes.mode_decompositions import PrincipalComponentsIndex, IndependentComponentsIndex
+from tvb.core.entities.model.datatypes.projections import ProjectionMatrixIndex
+from tvb.core.entities.model.datatypes.region_mapping import RegionVolumeMappingIndex, RegionMappingIndex
+from tvb.core.entities.model.datatypes.sensors import SensorsIndex
+from tvb.core.entities.model.datatypes.spectral import CoherenceSpectrumIndex, ComplexCoherenceSpectrumIndex, \
+    FourierSpectrumIndex, WaveletCoefficientsIndex
+from tvb.core.entities.model.datatypes.structural import StructuralMRIIndex
+from tvb.core.entities.model.datatypes.surface import SurfaceIndex
+from tvb.core.entities.model.datatypes.temporal_correlations import CrossCorrelationIndex
+from tvb.core.entities.model.datatypes.time_series import TimeSeriesIndex, TimeSeriesRegionIndex, \
+    TimeSeriesSurfaceIndex, TimeSeriesVolumeIndex, TimeSeriesEEGIndex, TimeSeriesMEGIndex, TimeSeriesSEEGIndex
+from tvb.core.entities.model.datatypes.tracts import TractsIndex
+from tvb.core.entities.model.datatypes.volume import VolumeIndex
 from tvb.interfaces.neocom._registry import Registry
 
 # an alternative approach is to make each h5file declare if it has a corresponding datatype
 # then in a metaclass hook each class creation and populate a map
 registry = Registry()
-registry.register_h5file_datatype(ConnectivityH5, Connectivity)
-registry.register_h5file_datatype(LocalConnectivityH5, LocalConnectivity)
-registry.register_h5file_datatype(ProjectionMatrixH5, ProjectionMatrix)
-registry.register_h5file_datatype(RegionVolumeMappingH5, RegionVolumeMapping)
-registry.register_h5file_datatype(RegionMappingH5, RegionMapping)
-registry.register_h5file_datatype(SensorsH5, Sensors)
-registry.register_h5file_datatype(SimulationStateH5, SimulationState)
-registry.register_h5file_datatype(CoherenceSpectrumH5, CoherenceSpectrum)
-registry.register_h5file_datatype(ComplexCoherenceSpectrumH5, ComplexCoherenceSpectrum)
-registry.register_h5file_datatype(FourierSpectrumH5, FourierSpectrum)
-registry.register_h5file_datatype(WaveletCoefficientsH5, WaveletCoefficients)
-registry.register_h5file_datatype(StructuralMRIH5, StructuralMRI)
-registry.register_h5file_datatype(SurfaceH5, Surface)
-registry.register_h5file_datatype(CrossCorrelationH5, CrossCorrelation)
-registry.register_h5file_datatype(TimeSeriesH5, TimeSeries)
-registry.register_h5file_datatype(TimeSeriesRegionH5, TimeSeriesRegion)
-registry.register_h5file_datatype(TimeSeriesSurfaceH5, TimeSeriesSurface)
-registry.register_h5file_datatype(TimeSeriesVolumeH5, TimeSeriesVolume)
-registry.register_h5file_datatype(TimeSeriesEEGH5, TimeSeriesEEG)
-registry.register_h5file_datatype(TimeSeriesMEGH5, TimeSeriesMEG)
-registry.register_h5file_datatype(TimeSeriesSEEGH5, TimeSeriesSEEG)
-registry.register_h5file_datatype(TractsH5, Tracts)
-registry.register_h5file_datatype(VolumeH5, Volume)
-registry.register_h5file_datatype(PrincipalComponentsH5, PrincipalComponents)
-registry.register_h5file_datatype(IndependentComponentsH5, IndependentComponents)
-registry.register_h5file_datatype(ConnectivityMeasureH5, ConnectivityMeasure)
+registry.register_h5file_datatype(ConnectivityH5, Connectivity, ConnectivityIndex)
+registry.register_h5file_datatype(LocalConnectivityH5, LocalConnectivity, LocalConnectivityIndex)
+registry.register_h5file_datatype(ProjectionMatrixH5, ProjectionMatrix, ProjectionMatrixIndex)
+registry.register_h5file_datatype(RegionVolumeMappingH5, RegionVolumeMapping, RegionVolumeMappingIndex)
+registry.register_h5file_datatype(RegionMappingH5, RegionMapping, RegionMappingIndex)
+registry.register_h5file_datatype(SensorsH5, Sensors, SensorsIndex)
+registry.register_h5file_datatype(SimulationStateH5, SimulationState, None)
+registry.register_h5file_datatype(CoherenceSpectrumH5, CoherenceSpectrum, CoherenceSpectrumIndex)
+registry.register_h5file_datatype(ComplexCoherenceSpectrumH5, ComplexCoherenceSpectrum, ComplexCoherenceSpectrumIndex)
+registry.register_h5file_datatype(FourierSpectrumH5, FourierSpectrum, FourierSpectrumIndex)
+registry.register_h5file_datatype(WaveletCoefficientsH5, WaveletCoefficients, WaveletCoefficientsIndex)
+registry.register_h5file_datatype(StructuralMRIH5, StructuralMRI, StructuralMRIIndex)
+registry.register_h5file_datatype(SurfaceH5, Surface, SurfaceIndex)
+registry.register_h5file_datatype(CrossCorrelationH5, CrossCorrelation, CrossCorrelationIndex)
+registry.register_h5file_datatype(TimeSeriesH5, TimeSeries, TimeSeriesIndex)
+registry.register_h5file_datatype(TimeSeriesRegionH5, TimeSeriesRegion, TimeSeriesRegionIndex)
+registry.register_h5file_datatype(TimeSeriesSurfaceH5, TimeSeriesSurface, TimeSeriesSurfaceIndex)
+registry.register_h5file_datatype(TimeSeriesVolumeH5, TimeSeriesVolume, TimeSeriesVolumeIndex)
+registry.register_h5file_datatype(TimeSeriesEEGH5, TimeSeriesEEG, TimeSeriesEEGIndex)
+registry.register_h5file_datatype(TimeSeriesMEGH5, TimeSeriesMEG, TimeSeriesMEGIndex)
+registry.register_h5file_datatype(TimeSeriesSEEGH5, TimeSeriesSEEG, TimeSeriesSEEGIndex)
+registry.register_h5file_datatype(TractsH5, Tracts, TractsIndex)
+registry.register_h5file_datatype(VolumeH5, Volume, VolumeIndex)
+registry.register_h5file_datatype(PrincipalComponentsH5, PrincipalComponents, PrincipalComponentsIndex)
+registry.register_h5file_datatype(IndependentComponentsH5, IndependentComponents, IndependentComponentsIndex)
+registry.register_h5file_datatype(ConnectivityMeasureH5, ConnectivityMeasure, ConnectivityMeasureIndex)
