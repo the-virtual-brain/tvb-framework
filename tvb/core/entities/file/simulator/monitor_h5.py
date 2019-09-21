79c5d40c4c2e20c862ec5f4e830ee12497656abc
liadomide <lia_domide@yahoo.com>
2019-09-21 11:10:00 +0300

    TVB-2512 Fix deprecation warnings in H5. Add license text. and fix typed

--- tvb/core/entities/file/simulator/monitor_h5.py
+++ tvb/core/entities/file/simulator/monitor_h5.py
@@ -1,5 +1,34 @@
-from tvb.simulator.monitors import Monitor, Raw, SpatialAverage, Projection, EEG, MEG, iEEG, Bold
+# -*- coding: utf-8 -*-
+#
+#
+# TheVirtualBrain-Framework Package. This package holds all Data Management, and
+# Web-UI helpful to run brain-simulations. To use it, you also need do download
+# TheVirtualBrain-Scientific Package (for simulators). See content of the
+# documentation-folder for more details. See also http://www.thevirtualbrain.org
+#
+# (c) 2012-2017, Baycrest Centre for Geriatric Care ("Baycrest") and others
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this
+# program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#   CITATION:
+# When using The Virtual Brain for scientific publications, please cite it as follows:
+#
+#   Paula Sanz Leon, Stuart A. Knock, M. Marmaduke Woodman, Lia Domide,
+#   Jochen Mersmann, Anthony R. McIntosh, Viktor Jirsa (2013)
+#       The Virtual Brain: a simulator of primate brain network dynamics.
+#   Frontiers in Neuroinformatics (7:10. doi: 10.3389/fninf.2013.00010)
+#
+#
 
+from tvb.simulator.monitors import Monitor, Raw, SpatialAverage, Projection, EEG, MEG, iEEG, Bold
 from tvb.core.entities.file.simulator.configurations_h5 import SimulatorConfigurationH5
 from tvb.core.neotraits.h5 import Scalar, DataSet, Reference
 
@@ -48,7 +77,7 @@ class ProjectionH5(MonitorH5):
         self.obnoise = Reference(Projection.obsnoise, self)
 
     def store(self, datatype, scalars_only=False, store_references=False):
-        # type: (Projection) -> None
+        # type: (Projection, bool, bool) -> None
         super(ProjectionH5, self).store(datatype, scalars_only, store_references)
         self.region_mapping.store(datatype.region_mapping.gid)
 
@@ -63,7 +92,7 @@ class EEGH5(ProjectionH5):
         self.sigma = Scalar(EEG.sigma, self)
 
     def store(self, datatype, scalars_only=False, store_references=False):
-        # type: (Projection) -> None
+        # type: (Projection, bool, bool) -> None
         super(EEGH5, self).store(datatype, scalars_only, store_references)
         self.projection.store(datatype.projection.gid)
         self.sensors.store(datatype.sensors.gid)
@@ -77,7 +106,7 @@ class MEGH5(ProjectionH5):
         self.sensors = Reference(MEG.sensors, self)
 
     def store(self, datatype, scalars_only=False, store_references=False):
-        # type: (Projection) -> None
+        # type: (Projection, bool, bool) -> None
         super(MEGH5, self).store(datatype, scalars_only, store_references)
         self.projection.store(datatype.projection.gid)
         self.sensors.store(datatype.sensors.gid)
@@ -92,7 +121,7 @@ class iEEGH5(ProjectionH5):
         self.sigma = Scalar(iEEG.sigma, self)
 
     def store(self, datatype, scalars_only=False, store_references=False):
-        # type: (Projection) -> None
+        # type: (Projection, bool, bool) -> None
         super(iEEGH5, self).store(datatype, scalars_only, store_references)
         self.projection.store(datatype.projection.gid)
         self.sensors.store(datatype.sensors.gid)
@@ -107,7 +136,7 @@ class BoldH5(MonitorH5):
         self.hrf_length = Scalar(Bold.hrf_length, self)
 
     def store(self, datatype, scalars_only=False, store_references=False):
-        # type: (Bold) -> None
+        # type: (Bold, bool, bool) -> None
         super(BoldH5, self).store(datatype, scalars_only, store_references)
         hrf_kernel_gid = self.store_config_as_reference(datatype.hrf_kernel)
         self.hrf_kernel.store(hrf_kernel_gid)
